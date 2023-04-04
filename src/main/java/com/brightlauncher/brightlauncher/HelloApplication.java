ba5f1bfe97f5b08c982f6b3f48246ddbdb229c39
Perfection <fructueuxjefferson@gmail.com>
2023-04-04 17:48:37 -0400

    Adding gitignore for java and intellij

--- src/main/java/com/brightlauncher/brightlauncher/HelloApplication.java
+++ src/main/java/com/brightlauncher/brightlauncher/HelloApplication.java
@@ -1,23 +1,45 @@
 package com.brightlauncher.brightlauncher;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
-public class HelloApplication extends Application {
+public class HelloApplication extends Application{
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) {
+
+        ButtonBar br = new ButtonBar();
+        br.getButtons().add(new Button("Hey"));
+        //Creating a line object
+        Line line = new Line();
+
+        //Setting the properties to a line
+        line.setStartX(100.0);
+        line.setStartY(150.0);
+        line.setEndX(500.0);
+        line.setEndY(150.0);
+
+        //Creating a Group
+        Group root = new Group();
+        root.getChildren().add(br);
+
+        //Creating a Scene
+        Scene scene = new Scene(root, 600, 300);
+
+        //Setting title to the scene
+        stage.setTitle("Sample application");
+
+        //Adding the scene to the stage
         stage.setScene(scene);
+
+        //Displaying the contents of a scene
         stage.show();
     }
-
-    public static void main(String[] args) {
-        launch();
+    public static void main(String args[]){
+        launch(args);
     }
 }
